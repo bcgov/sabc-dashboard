5d0a738325c38118af16618899da09956f84ea02
Kal Marsh <kal.marsh@gov.bc.ca>
2024-10-28 09:46:55 -0700

    adds more text changes to the login page

--- resources/js/components/auth/DashboardLogin.vue
+++ resources/js/components/auth/DashboardLogin.vue
@@ -9,16 +9,14 @@
                 <p>Please select the option below that best applies to you.</p>
                 <div v-if="maintenanceMode === false" class="card mb-3">
                     <div class="card-header text-dark bg-light">
-                        <span class="text-primary text-left p-0">Full-Time Students</span>
+                        <span class="text-primary text-left p-0">Full-Time Student</span>
                     </div>
                     <div class="collapse show">
                         <div class="card-body">
 
                             <div class="form-group row mb-0">
                                 <div class="col-12">
-                                    <p>A student who is enrolled in a minimum of 60 percent full course load (40 percent
-                                        for students with a permanent disability, or a persistent or prolonged disability)
-                                        for at least 12 consecutive weeks at a designated post-secondary school.</p>
+                                    <p>A student who is enrolled in a minimum of 60 percent of a full course load (40 percent for students with a permanent disability, or a persistent or prolonged disability) at a designated public or private post-secondary institution.</p>
 
                                     <div class="accordion mb-2" id="accordionA">
                                         <div class="card">
@@ -30,7 +28,7 @@
 
                                     <div class="accordion mb-2" id="accordionC">
                                         <div class="card">
-                                            <a href="https://id.gov.bc.ca/account/" class="btn btn-link btn-block text-center p-3" target="_blank">I don't have a BC Services Card account. How can I get one?</a>
+                                            <a href="https://id.gov.bc.ca/account/" class="btn btn-link btn-block text-center p-3" target="_blank">Learn how to use a BC Services Card account to log in</a>
                                         </div>
                                     </div>
 
@@ -39,13 +37,15 @@
                                             <div class="card-header p-2" id="headingB">
                                                 <h2 class="mb-0">
                                                     <button @click="toggleCollapse('collapseB')" class="btn btn-link btn-block text-center" type="button">
-                                                        Log in with a StudentAid BC User ID
+                                                        I’m not eligible for a BC Services Card Account. What can I do?
                                                     </button>
                                                 </h2>
                                             </div>
 
                                             <div id="collapseB" class="collapse" aria-labelledby="headingB" data-parent="#accordionB">
                                                 <div class="card-body">
+                                                    <p>Using a BC Services Card account is the easiest, most secure way to apply for student financial aid. However, we understand not everyone is eligible for the BC Services Card account. To inquire about alternate login options, please email a request to <a href="mailto:StudentAidBC@gov.bc.ca" target="_blank">StudentAidBC@gov.bc.ca</a> and provide a detailed explanation why you are not eligible for a BC Services Card account. StudentAid BC will review your request and provide further instructions via email.</p>
+                                                    <label class="mb-3 mt-3">Log in with StudentAid BC User ID</label>
                                                     <form method="POST" action="/dashboard/login">
                                                         <input type="hidden" name="_token" :value="csrf">
                                                         <input type="hidden" name="trap" value="">
@@ -100,16 +100,14 @@
 
                 <div v-if="maintenanceMode === false" class="card mb-3">
                     <div class="card-header text-dark bg-light">
-                        <span class="text-primary text-left p-0">Part-Time Students</span>
+                        <span class="text-primary text-left p-0">Part-Time Student</span>
                     </div>
                     <div class="collapse show">
                         <div class="card-body">
 
                             <div class="form-group row mb-0">
                                 <div class="col-12">
-                                    <p>A student who is enrolled in a 20 to 59 percent of a full-time course load (20 to 39 percent
-                                        for students with a permanent disability, or a persistent or prolonged disability)
-                                        at a public or private designated post-secondary school.</p>
+                                    <p>A student who is enrolled in at least 20 to 59 percent of a full course load at a designated public or private post-secondary institution.</p>
 
                                     <div class="accordion mb-2" id="accordionA2">
                                         <div class="card">
@@ -124,6 +122,9 @@
                 </div>
 
 
+                <div v-if="maintenanceMode === false" class="p-4">
+                    <p><strong>Minimum system requirements. </strong>Please note that our full-time and part-time applications are not compatible with mobile devices, including smartphones and tablets. Please check that your computer/device meets the <strong><a href="/help-centre/loan-application-instructions/minimum-system-requirements" target="_blank">minimum system requirements</a></strong>.</p>
+                </div>
                 <div v-if="maintenanceMode === false" class="p-4">
                     <small>Collection and Use of Information. The information included in this form is collected under ss. 26(c) and 26(e) of the Freedom of Information and Protection of Privacy Act, R.S.B.C. 1996, c. 165. Upon pressing the “Submit” button you are confirming that you have reviewed this statement. The information you provide will be used in confirming your identity. If you have any questions about the collection and use of this information, contact the Executive Director, StudentAid BC, Ministry of Post-Secondary Education and Future Skills, PO Box 9173, Stn Prov Govt, Victoria BC, V8W 9H7, telephone 1-800-561-1818 (toll-free in Canada/U.S.) or +1-778-309-4621 from outside North America.</small>
                 </div>
